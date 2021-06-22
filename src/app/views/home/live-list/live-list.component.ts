d40a930c23ac4b03bf9a7a2e05dcaefcdf354326
Pablo Gonzalez <pablojava853@outlook.com>
2021-06-22 00:43:54 -0300

    Alterações no live-list.componet - formatação dos cards da live

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -9,12 +9,23 @@ import { Live } from 'src/app/shared/model/live.model';
 })
 export class LiveListComponent implements OnInit {
 
-  livesPrevious!: Live[];
+  livesPrevious: Live[] = [];
+  livesNext: Live[] = [];
   constructor(
     public liveService: LiveService
   ) { }
 
   ngOnInit(): void {
+    this.getLives();
   }
-
+getLives(){
+  this.liveService.getLivesWithFlag('previous').subscribe(data => {
+    this.livesPrevious = data.content;
+    console.log(this.livesPrevious);
+  });
+  
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+    });
+  }
 }
